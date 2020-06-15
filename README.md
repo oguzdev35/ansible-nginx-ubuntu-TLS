94e669af57071d127ca76c0298f1516041dc0c63
Oğuz Kurttutan <oguzdev35@gmail.com>
2020-06-15 10:02:32 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Commands
+./generate_tls.file.sh
+ansible-playbook web-tls.yml
---
+++ generate_tls_file.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir files && \
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
+  -subj /CN=localhost \
+  -keyout files/nginx.key -out files/nginx.crt
